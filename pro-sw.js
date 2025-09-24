d3788d8dccfaf1f9921e168a6c09b6eb8372cd8a
Adam Kaminski <23adamkaminski@gmail.com>
2025-09-23 14:36:56 -1000

    Updates

--- pro-sw.js
+++ pro-sw.js
@@ -1,5 +1,5 @@
 // Pomodoro Pro Service Worker - iOS Safari Compatible
-const CACHE_NAME = 'pomodoro-pro-v4';
+const CACHE_NAME = 'pomodoro-pro-v5';
 const urlsToCache = [
   '/pomodoro-pro/pro.html',
   '/pomodoro-pro/pro-manifest.json',
